990eceae75440f101fe900dcc986061fe214bd45
MaXx S <maxxamillion04@gmail.com>
2024-12-13 12:32:35 -0600

    Initial Commit

---
+++ FetchTakehomeUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchTakehomeUIKit
+//
+//  Created by MaXx Speller on 12/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
